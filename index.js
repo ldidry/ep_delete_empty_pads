0885fab1c0b93d3c67a1e4eb5580a43992ac2ed8
Luc Didry <luc@didry.org>
2015-03-12 20:58:50 +0100

    First import, working

---
+++ index.js
@@ -0,0 +1,42 @@
+var API        = require('ep_etherpad-lite/node/db/API'),
+    ERR        = require('ep_etherpad-lite/node_modules/async-stacktrace'),
+    async      = require('ep_etherpad-lite/node_modules/async');
+
+// Check if we need to delete the pad each time a user leaves
+exports.deletePadAtLeave = function(hook, session, cb) {
+    var pad = session.padId;
+    API.getRevisionsCount(pad, function(err, d) {
+        if (d.revisions === 0) {
+            API.deletePad(pad, function(err, d) {
+                console.log('Deleting %s when user leaved since empty', session.padId);
+                cb && cb();
+            });
+        } else {
+            cb && cb();
+        }
+    });
+};
+
+// Delete empty pads at startup
+exports.deletePadsAtStart = function (hook_name, args, cb) {
+    var i = 0;
+    // Deletion queue (avoids max stack size error)
+    var q = async.queue(function (pad, cb) {
+        API.deletePad(pad, function(err, d) {
+            cb && cb();
+        });
+    }, 2);
+    API.listAllPads(function(err, data) {
+        async.each(data.padIDs, function(pad, callback) {
+            API.getRevisionsCount(pad, function(err, d) {
+                if (d.revisions === 0) {
+                    // Enqueue the pad for deletion
+                    q.push(pad, function(err) {
+                        console.log('Deleting %s at startup since empty', pad);
+                    });
+                }
+            });
+        });
+        cb && cb();
+    });
+};
