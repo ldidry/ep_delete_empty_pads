a1fa17763ea4e51149ec4cb7892dd1148426e89f
Fabien Bourgeois <fabien@yaltik.com>
2015-03-17 10:20:26 +0100

    Simplification using direct internal etherpad functions; Callback errors handling

--- index.js
+++ index.js
@@ -1,49 +1,34 @@
-var API        = require('ep_etherpad-lite/node/db/API'),
-    PadManager = require('ep_etherpad-lite/node/db/PadManager'),
-    ERR        = require('ep_etherpad-lite/node_modules/async-stacktrace'),
-    async      = require('ep_etherpad-lite/node_modules/async');
+var  PadManager = require('ep_etherpad-lite/node/db/PadManager'),
+  async      = require('ep_etherpad-lite/node_modules/async');
 
 // Check if we need to delete the pad each time a user leaves
 exports.deletePadAtLeave = function(hook, session, cb) {
-    var pad = session.padId;
-    PadManager.doesPadExists(pad, function(err, exists) {
-        if (exists) {
-            API.getRevisionsCount(pad, function(err, d) {
-                if (d.revisions === 0) {
-                    API.deletePad(pad, function(err, d) {
-                        console.log('Deleting %s when user leaved since empty', session.padId);
-                        cb && cb();
-                    });
-                } else {
-                    cb && cb();
-                }
-            });
-        } else {
-            cb && cb();
-        }
-    });
+  PadManager.getPad(session.padId, function (err, pad) {
+    if (err) { return cb(err); }
+    if (pad.getHeadRevisionNumber() === 0) {
+      console.log('Deleting %s when user leaved since empty', session.padId);
+      pad.remove(cb);
+    } else {
+      cb();
+    }
+  });
 };
 
 // Delete empty pads at startup
 exports.deletePadsAtStart = function (hook_name, args, cb) {
-    var i = 0;
-    // Deletion queue (avoids max stack size error)
-    var q = async.queue(function (pad, cb) {
-        API.deletePad(pad, function(err, d) {
-            cb && cb();
-        });
-    }, 2);
-    API.listAllPads(function(err, data) {
-        async.each(data.padIDs, function(pad, callback) {
-            API.getRevisionsCount(pad, function(err, d) {
-                if (d.revisions === 0) {
-                    // Enqueue the pad for deletion
-                    q.push(pad, function(err) {
-                        console.log('Deleting %s at startup since empty', pad);
-                    });
-                }
-            });
-        });
-        cb && cb();
+  // Deletion queue (avoids max stack size error), 4 workers
+  var q = async.queue(function (pad, callback) { pad.remove(callback); }, 4);
+  PadManager.listAllPads(function (err, data) {
+    async.each(data.padIDs, function (padId, cb) {
+      PadManager.getPad(padId, function (err, pad) {
+        if (err) { return cb(err); }
+        if (pad.getHeadRevisionNumber() === 0) {
+          q.push(pad, function (err) {
+            if (err) { return cb(err); }
+            console.log('Deleting %s at startup since empty', pad.id);
+          });
+        }
+      });
     });
+  });
 };
