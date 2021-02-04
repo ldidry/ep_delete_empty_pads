6da46106db85ff271c5f45a4eb5601e5ce654a36
Luc Didry <luc@didry.org>
2021-02-04 16:52:48 +0100

    Adapt for Etherpad 1.8.7

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const log4js = require('ep_etherpad-lite/node_modules/log4js');
+const logger = log4js.getLogger('ep_delete_empty_pads');
+
 var PadManager = require('ep_etherpad-lite/node/db/PadManager'),
-    async      = require('ep_etherpad-lite/node_modules/async');
+    asyncM     = require('ep_etherpad-lite/node_modules/async');
 
 var epVersion = parseFloat(require('ep_etherpad-lite/package.json').version);
 var usePromises = epVersion >= 1.8
@@ -16,56 +19,61 @@ if (usePromises) {
 }
 
 // Check if we need to delete the pad each time a user leaves
-exports.deletePadAtLeave = function(hook, session, cb) {
+exports.deletePadAtLeave = (hook, session, cb) => {
     if (session !== undefined && session !== null) {
         var pad = session.padId;
-        doesPadExists(pad, function(err, exists) {
+        doesPadExists(pad, (err, exists) => {
             if (exists !== undefined && exists !== null) {
                 if (exists) {
-                    getPad(pad, null, function (err, pad) {
-                        if (err) {
+                    getPad(pad, null, (err, pad) => {
+                        if (err && !usePromises) {
                             return cb(err);
                         }
                         var head = pad.getHeadRevisionNumber();
                         if (head !== undefined && head !== null) {
                             if (head === 0) {
-                                console.log('Deleting %s when user leaved since empty', session.padId);
+                                logger.info('Deleting '+session.padId+' when user leaved since empty');
                                 var remove = getRemoveFun(pad)
-                                remove(cb);
-                            } else {
+                                if (usePromises) {
+                                    remove(() => {});
+                                } else {
+                                    remove(cb);
+                                }
+                            } else if (!usePromises) {
                                 cb();
                             }
                         }
                     });
-                } else {
+                } else if (!usePromises) {
                     cb();
                 }
             }
         });
     }
+    if (usePromises) { return cb(); }
 };
 
 // Delete empty pads at startup
-exports.deletePadsAtStart = function (hook_name, args, cb) {
+exports.deletePadsAtStart = (hook_name, args, cb) => {
     // Deletion queue (avoids max stack size error), 2 workers
-    var q = async.queue(function (pad, callback) {
+    var q = asyncM.queue((pad, callback) => {
         var remove = getRemoveFun(pad)
         remove(callback);
     }, 2);
     // Emptyness test queue
-    var p = async.queue(function(padId, callback) {
-        getPad(padId, null, function (err, pad) {
+    var p = asyncM.queue((padId, callback) => {
+        getPad(padId, null, (err, pad) => {
             if (err) {
                 return callback(err);
             }
             var head = pad.getHeadRevisionNumber();
             if (head !== undefined && head !== null) {
                 if (head === 0) {
-                    q.push(pad, function (err) {
+                    q.push(pad, (err) => {
                         if (err) {
                             return callback(err);
                         }
-                        console.log('Deleting %s at startup since empty', pad.id);
+                        logger.info('Deleting '+pad.id+' at startup since empty');
                     });
                 }
                 callback();
@@ -73,37 +81,38 @@ exports.deletePadsAtStart = function (hook_name, args, cb) {
         });
     }, 1);
 
-    listAllPads(function (err, data) {
+    listAllPads((err, data) => {
         for (var i = 0; i < data.padIDs.length; i++) {
             var padId = data.padIDs[i];
-            p.push(padId, function(err) {
-                if (err) {
+            p.push(padId, (err) => {
+                if (err && !usePromises) {
                     return cb(err);
                 }
             });
         }
     });
+    if (usePromises) { return cb(); }
 };
 
 function wrapPromise (p, cb) {
-  return p.then(function (result) { cb(null, result); })
-    .catch(function(err) { cb(err); });
+  return p.then((result) => { cb(null, result); })
+    .catch((err) => { cb(err); });
 }
 
 function callbackify0 (fun) {
-  return function (cb) {
+  return (cb) => {
     return wrapPromise(fun(), cb);
   };
 };
 
 function callbackify1 (fun) {
-  return function (arg1, cb) {
+  return (arg1, cb) => {
     return wrapPromise(fun(arg1), cb);
   };
 };
 
 function callbackify2 (fun) {
-  return function (arg1, arg2, cb) {
+  return (arg1, arg2, cb) => {
     return wrapPromise(fun(arg1, arg2), cb);
   };
 };
@@ -115,5 +124,5 @@ function getRemoveFun (pad) {
     return callbackify0(fun)
   }
 
-  return fun
+  return fun;
 }
