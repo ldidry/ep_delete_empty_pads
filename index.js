a213a9e678cbe8653fa86ed3267fc95ff4d11ead
Richard Hansen <rhansen@rhansen.org>
2021-06-17 04:20:58 -0400

    Minimize time between revision check and deletion
    This fixes a race condition (deleting a pad that is edited shortly
    after the revision check).

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+const PadManager = require('ep_etherpad-lite/node/db/PadManager');
 const log4js = require('ep_etherpad-lite/node_modules/log4js');
-const logger = log4js.getLogger('ep_delete_empty_pads');
 
-var PadManager = require('ep_etherpad-lite/node/db/PadManager'),
-    asyncM     = require('ep_etherpad-lite/node_modules/async');
+const logger = log4js.getLogger('ep_delete_empty_pads');
 
 // Check if we need to delete the pad each time a user leaves
 exports.deletePadAtLeave = (hook, session, cb) => {
@@ -20,17 +19,13 @@ exports.deletePadAtLeave = (hook, session, cb) => {
 // Delete empty pads at startup
 exports.deletePadsAtStart = (hook_name, args, cb) => {
     (async () => {
-        // Deletion queue (avoids max stack size error), 2 workers
-        const q = asyncM.queue(async (pad) => {
-            logger.info(`Deleting ${pad.id} at startup since empty`);
-            await pad.remove();
-        }, 2);
-
         const {padIDs} = await PadManager.listAllPads();
+        // Process pads one at a time to avoid putting too much load on the system.
         for (const padId of padIDs) {
             const pad = await PadManager.getPad(padId);
             if (pad.getHeadRevisionNumber() !== 0) continue;
-            q.push(pad);
+            logger.info(`Deleting ${pad.id} at startup since empty`);
+            await pad.remove();
         }
     })();
     return cb(); // No need to wait for completion before calling the callback.
